b2dd635359c495c2ddbc871685628137ccc7df01
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-15 20:52:16 -0300

    feat: registrando rotas de categorias.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.routes.category_routes import router as category_routes
 
 
 app = FastAPI()
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get('/health-check')
 def health_check():
     return True
+
+
+app.include_router(category_routes)
