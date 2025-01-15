6e97579c9b4d1f7c6e6bce009ca577a638a34f81
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-15 20:06:09 -0300

    feat: adicionando conftest para criação de fixtures do pytest.

---
+++ app/test/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from app.db.connection import Session
+
+
+@pytest.fixture()
+def db_session():
+    try:
+        session = Session()
+        
+        yield session
+    finally:
+        session.close()
