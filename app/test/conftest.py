53d78e767c4db2dddaf109e874a866f4ab557f88
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-15 21:05:21 -0300

    feat: criando fixture para categorias no banco de dados.

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app.db.connection import Session
+from app.db.models import Category as CategoryModel
 
 
 @pytest.fixture()
@@ -10,3 +11,26 @@ def db_session():
         yield session
     finally:
         session.close()
+
+
+@pytest.fixture()
+def categories_on_db(db_session):
+    categories = [
+        CategoryModel(name="Roupa", slug="roupa"),
+        CategoryModel(name="Carro", slug="carro"),
+        CategoryModel(name="Itens de cozinha", slug="itens-de-cozinha"),
+        CategoryModel(name="Decoracao", slug="decoracao"),
+    ]
+
+    for category in categories:
+        db_session.add(category)
+    db_session.commit()
+
+    for category in categories:
+        db_session.refresh(category)
+
+    yield categories
+
+    for category in categories:
+        db_session.delete(category)
+    db_session.commit()
