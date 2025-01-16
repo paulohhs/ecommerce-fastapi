d37573a6d7fd70c2c4ef87de47e5b17320bdd90d
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-15 21:13:52 -0300

    feat: adicionando caso de uso para listagem de categorias.

--- app/use_cases/category.py
+++ app/use_cases/category.py
@@ -1,14 +1,26 @@
 from sqlalchemy.orm import Session
 from app.db.models import Category as CategoryModel
-from app.schemas.category import Category
+from app.schemas.category import Category, CategoryOutput
 
 
 class CategoryUseCases:
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
+    def serialize_category(self, category_model: CategoryModel):
+        return CategoryOutput(**category_model.__dict__)
+
     def add_category(self, category: Category):
         category_model = CategoryModel(**category.dict())
 
         self.db_session.add(category_model)
         self.db_session.commit()
+
+    def list_categories(self):
+        categories_on_db = self.db_session.query(CategoryModel).all()
+        categories_output = [
+            self.serialize_category(category_model)
+            for category_model in categories_on_db
+        ]
+
+        return categories_output
