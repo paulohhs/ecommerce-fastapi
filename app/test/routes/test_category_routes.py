04bf267d9fdb24baa58c904b81e4648c878276d6
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-15 21:19:31 -0300

    feat: adicionando teste para rota de listagem de categorias.

--- app/test/routes/test_category_routes.py
+++ app/test/routes/test_category_routes.py
@@ -21,3 +21,18 @@ def test_add_category_route(db_session):
 
     db_session.delete(categories_on_db[0])
     db_session.commit()
+
+
+def test_list_categories_route(categories_on_db):
+    response = client.get('/category/list')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+
+    assert len(data) == 4
+    assert data[0] == {
+        "id": categories_on_db[0].id,
+        "name": categories_on_db[0].name,
+        "slug": categories_on_db[0].slug,
+    }
