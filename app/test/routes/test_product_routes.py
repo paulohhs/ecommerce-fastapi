6fa58a8e50852afb3cb78533698f0411a90c6289
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-16 22:32:49 -0300

    tests: adicionando teste para rota de atualização de produtos.

--- app/test/routes/test_product_routes.py
+++ app/test/routes/test_product_routes.py
@@ -44,3 +44,36 @@ def test_add_product_route_invalid_category_slug(db_session):
 
     products_on_db = db_session.query(ProductModel).all()
     assert len(products_on_db) == 0
+
+
+def test_update_product_route(db_session, product_on_db):
+    body = {
+        "name": "Update Camisa",
+        "slug": "update-camisa",
+        "price": 23.88,
+        "stock": 10
+    }
+
+    response = client.put(f'/product/update/{product_on_db.id}', json=body)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    db_session.refresh(product_on_db)
+
+    assert product_on_db.name == "Update Camisa"
+    assert product_on_db.slug == "update-camisa"
+    assert product_on_db.price == 23.88
+    assert product_on_db.stock == 10
+
+
+def test_update_product_route_invalid_id(db_session):
+    body = {
+        "name": "Update Camisa",
+        "slug": "update-camisa",
+        "price": 23.88,
+        "stock": 10
+    }
+
+    response = client.put('/product/update/1', json=body)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
