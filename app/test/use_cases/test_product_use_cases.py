56e9fc71c5de42f7b76d42152e3c3da95f8a89a8
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-21 19:14:33 -0300

    testes: adicionando teste para o caso de uso de listagem de produtos.

--- app/test/use_cases/test_product_use_cases.py
+++ app/test/use_cases/test_product_use_cases.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.exceptions import HTTPException
 from app.db.models import Product as ProductModel
-from app.schemas.product import Product
+from app.schemas.product import Product, ProductOutput
 from app.use_cases.product import ProductUseCases
 
 
@@ -92,3 +92,20 @@ def test_delete_product_uc_non_exist(db_session):
     with pytest.raises(HTTPException):
         uc.delete_product(id=1)
 
+
+def test_list_products_uc(db_session, products_on_db):
+    uc = ProductUseCases(db_session=db_session)
+
+    products = uc.list_products()
+
+    for product in products_on_db:
+        db_session.refresh(product)
+
+    assert len(products) == 4
+    assert type(products[0]) == ProductOutput
+    assert products[0].id == products_on_db[0].id
+    assert products[0].name == products_on_db[0].name
+    assert products[0].slug == products_on_db[0].slug
+    assert products[0].price == products_on_db[0].price
+    assert products[0].stock == products_on_db[0].stock
+    assert products[0].category.name == products_on_db[0].category.name
