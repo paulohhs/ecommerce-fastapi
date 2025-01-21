4fb4791c20419a3a37052e3367fc13547f5027fc
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-21 19:24:14 -0300

    testes: adicionando testes para a rota de listagem de produtos.

--- app/test/routes/test_product_routes.py
+++ app/test/routes/test_product_routes.py
@@ -87,6 +87,27 @@ def test_delete_product_route(db_session, product_on_db):
     assert len(products_on_db) == 0
 
 
-def test_delete_product_route_invalid_id(product_on_db):
+def test_delete_product_route_invalid_id():
     response = client.delete('/product/delete/2')
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_list_products_route(products_on_db):
+    response = client.get('/product/list')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+
+    assert len(data) == 4
+    assert data[0] == {
+        "id": products_on_db[0].id,
+        "name": products_on_db[0].name,
+        "slug": products_on_db[0].slug,
+        "price": products_on_db[0].price,
+        "stock": products_on_db[0].stock,
+        "category": {
+            "name": products_on_db[0].category.name,
+            "slug": products_on_db[0].category.slug
+        }
+    }
