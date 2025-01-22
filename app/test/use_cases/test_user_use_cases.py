dc49e3dbc6954be55ef8a3b4bdf4c8e825b728bb
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-22 20:09:50 -0300

    testes: adiciona teste para caso de uso de cadastro de usuário.

---
+++ app/test/use_cases/test_user_use_cases.py
@@ -0,0 +1,39 @@
+import pytest
+from passlib.context import CryptContext
+from fastapi.exceptions import HTTPException
+from app.schemas.user import User
+from app.db.models import User as UserModel
+from app.use_cases.user import UserUseCases
+
+
+crypt_context = CryptContext(schemes=['sha256_crypt'])
+
+
+def test_register_user_uc(db_session):
+    user = User(
+        username="UsuarioTeste",
+        password="pass#"
+    )
+
+    uc = UserUseCases(db_session=db_session)
+    uc.register_user(user=user)
+
+    user_on_db = db_session.query(UserModel).first()
+    assert user_on_db is not None
+    assert user_on_db.username == user.username
+    assert crypt_context.verify(user.password, user_on_db.password)
+
+    db_session.delete(user_on_db)
+    db_session.commit()
+
+
+def test_register_user_uc_username_already_exists(db_session, user_on_db):
+    user = User(
+        username=user_on_db.username,
+        password=crypt_context.hash("pass#")
+    )
+
+    uc = UserUseCases(db_session=db_session)
+
+    with pytest.raises(HTTPException):
+        uc.register_user(user=user)
