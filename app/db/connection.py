4069e90a24a45e9af17741db909b385818d8e932
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-21 21:01:17 -0300

    refactor: configurando banco de dados de testes.

--- app/db/connection.py
+++ app/db/connection.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DB_URL = config('DB_URL')
+TEST_MODE = config('TEST_MODE', default=False, cast=bool)
+DB_URL = config('DB_URL_TEST') if TEST_MODE else config('DB_URL')
 
 engine = create_engine(DB_URL, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
