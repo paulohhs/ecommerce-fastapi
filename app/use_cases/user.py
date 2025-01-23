dbcbd671c57112e3e2106b3393dafdc04b7a8631
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-22 22:57:08 -0300

    feat: adiciona caso de uso para login de usuário.

--- app/use_cases/user.py
+++ app/use_cases/user.py
@@ -1,13 +1,19 @@
+from decouple import config
+from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from fastapi.exceptions import HTTPException
 from fastapi import status
-from app.schemas.user import User
+from app.schemas.user import User, TokenData
 from app.db.models import User as UserModel
 
 
 crypt_context = CryptContext(schemes=['sha256_crypt'])
+
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
 
 
 class UserUseCases:
@@ -24,6 +30,33 @@ class UserUseCases:
 
         try:
             self.db_session.commit()
-        except:
+        except IntegrityError:
             self.db_session.rollback()
+            
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
+        
+    def user_login(self, user: User, expires_in: int = 30):
+        user_on_db = self._get_user(username=user.username)
+
+        if user_on_db is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password does not exist")
+        
+        if not crypt_context.verify(user.password, user_on_db.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password does not exist")
+        
+        expires_at = datetime.utcnow() + timedelta(minutes=expires_in)
+
+        data = {
+            "sub": user.username,
+            "exp": expires_at
+        }
+
+        access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+        token_data = TokenData(access_token=access_token, expires_at=expires_at)
+
+        return token_data
+    
+    def _get_user(self, username: str):
+        user_on_db = self.db_session.query(UserModel).filter_by(username=username).first()
+
+        return user_on_db
