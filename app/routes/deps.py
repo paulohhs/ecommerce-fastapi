57087111c776a247671feccde30b5669371520ce
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-23 21:36:47 -0300

    feat: criando dependência de autenticação.

--- app/routes/deps.py
+++ app/routes/deps.py
@@ -1,4 +1,13 @@
+from decouple import config
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 from app.db.connection import Session
+from app.use_cases.user import UserUseCases
+
+
+oauth_schema = OAuth2PasswordBearer(tokenUrl="/user/login")
+TEST_MODE = config('TEST_MODE', default=False, cast=bool)
 
 
 def get_db_session():
@@ -8,3 +17,14 @@ def get_db_session():
         yield session
     finally:
         session.close()
+
+
+def auth(
+    db_session: Session = Depends(get_db_session),
+    token = Depends(oauth_schema)
+):
+    if TEST_MODE:
+        return
+    
+    uc = UserUseCases(db_session=db_session)
+    uc.verify_token(token=token)
