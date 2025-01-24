ac078454a205fa76181ae49e82d591041a1d730b
Paulo Honorato <paulohhonoratos@gmail.com>
2025-01-23 21:02:48 -0300

    testes: adicionando teste para caso de uso de verificação do token.

--- app/test/use_cases/test_user_use_cases.py
+++ app/test/use_cases/test_user_use_cases.py
@@ -1,6 +1,8 @@
 import pytest
+from decouple import config
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from jose import jwt
 from fastapi.exceptions import HTTPException
 from app.schemas.user import User
 from app.db.models import User as UserModel
@@ -8,6 +10,9 @@ from app.use_cases.user import UserUseCases
 
 
 crypt_context = CryptContext(schemes=['sha256_crypt'])
+
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
 
 
 def test_register_user_uc(db_session):
@@ -75,3 +80,30 @@ def test_user_login_uc_invalid_password(db_session, user_on_db):
 
     with pytest.raises(HTTPException):
         token_data = uc.user_login(user=user, expires_in=30)
+
+
+def test_verify_token(db_session, user_on_db):
+    uc = UserUseCases(db_session=db_session)
+
+    data = {
+        "sub": user_on_db.username,
+        "exp": datetime.utcnow() + timedelta(minutes=30)
+    }
+
+    access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+    uc.verify_token(token=access_token)
+
+
+def test_verify_token_expired(db_session, user_on_db):
+    uc = UserUseCases(db_session=db_session)
+
+    data = {
+        "sub": user_on_db.username,
+        "exp": datetime.utcnow() - timedelta(minutes=30)
+    }
+
+    access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException):
+        uc.verify_token(token=access_token)
